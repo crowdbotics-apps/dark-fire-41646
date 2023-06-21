6e2d22b74c62d7797c18ecb61c76829064f505c5
Matt Peterson <revcompgeek@gmail.com>
2023-06-20 22:24:20 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import { ImageBackground } from "react-native";
+import { StyleSheet } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView } from "react-native";
+import { styles } from "./styles";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
@@ -14,9 +15,15 @@ const WelcomeScreen = () => {
           </Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+      <ImageBackground style={_styles.bXZnBdBo}></ImageBackground></ScrollView>
+    </SafeAreaView>;
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
+
+const _styles = StyleSheet.create({
+  bXZnBdBo: {
+    width: 107,
+    height: 62
+  }
+});
